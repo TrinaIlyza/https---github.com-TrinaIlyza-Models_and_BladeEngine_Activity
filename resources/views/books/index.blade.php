eea5b9572178739a5e1b9ac70ca00011c35e03f0
Trina Ilyza <tds4428@students.uc-bcf.edu.ph>
2025-04-08 20:55:21 +0800

    fixing bugs

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -9,7 +9,7 @@
 <body>
     <!-- Header -->
     <header class="bg-blue-500 p-4 text-white">
-        <h1 class="text-2xl">Books List</h1>
+        <h1 class="text-2xl">Books Lists</h1>
     </header>
 
     <!-- Book List -->
